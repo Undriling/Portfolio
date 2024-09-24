3b7d1cbc4f84a0048865ce37a366e3645026e01e
Manash <manashbaruah219@gmail.com>
2024-09-24 23:17:27 +0530

    init

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask,render_template,jsonify
+
+PROJECTS=[
+    {
+        "id": 1,
+        "title": "Password Manager",
+        "lang": "Python"
+    },
+    {
+        "id": 2,
+        "title": "Quiz Management System like KBC",
+        "lang": "Python"
+    },
+    {
+        "id": 3,
+        "title": "Multi App friendly QR Code Generator",
+        "lang": "Python"
+    },
+    {
+        "id": 4,
+        "title": "Rent Calculator",
+        "lang": "Python"
+    }
+]
+
+# Social Item 
+SOCIALS=[
+    {
+        "id": 1,
+        "title": "Linkdin",
+        "link": "https://www.linkedin.com/in/manash-baruah-mb",
+        "bt_icon": "  .in  "
+    },
+    {
+        "id": 2,
+        "title": "Github",
+        "link": "https://github.com/Undriling",
+        "bt_icon": "git"
+    },
+    {
+        "id": 3,
+        "title": "Twitter",
+        "link": "https://x.com/ManashBaruah22",
+        "bt_icon": "  X  "
+    }
+]
+
+app = Flask(__name__)
+@app.route('/')
+def hello_manash():
+    return render_template('home.html', projects=PROJECTS, socials=SOCIALS)
+
+# API 
+@app.route('/api/projects')
+def list_projects():
+    return jsonify(PROJECTS)
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
